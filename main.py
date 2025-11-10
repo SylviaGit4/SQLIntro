83b84dda0d26218c44d71443b3253e61c5ab8d9b
SylviaGit4 <torchicky139@gmail.com>
2025-11-10 15:48:28 +0000

    Protected page

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, url_for, render_template, redirect, session
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired
-from flask_login import LoginManager
+from flask_login import LoginManager, login_required
 
 #from flask_bootstrap import Bootstrap5
 
@@ -13,6 +13,16 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 
 #bootstrap = Bootstrap5(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.get(user_id)
+
+
 
 app.config['SECRET_KEY'] = 'key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
@@ -79,11 +89,18 @@ def login():
         password = form.password.data
         
         if user and user.password == password:
-            return redirect(url_for('index'))
+            load_user(Users.id)
+            return redirect(url_for('protected'))
         else:
             loginError = "Invalid email or password."
 
     return render_template('login.html', form=form, loginError=loginError)
+
+@app.route('/protected')
+@login_required
+def protected():
+    return render_template('protected.html')
+
 
 
 if __name__ == '__main__':
