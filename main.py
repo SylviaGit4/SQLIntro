148483366e280dd328ccdda47b6068ec80667623
SylviaGit4 <torchicky139@gmail.com>
2025-11-11 13:38:00 +0000

    Frontend.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired
 from flask_login import LoginManager, login_required, login_user
 
-#from flask_bootstrap import Bootstrap5
+from flask_bootstrap import Bootstrap5
 
 import os
 from flask_sqlalchemy import SQLAlchemy
@@ -12,7 +12,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 
-#bootstrap = Bootstrap5(app)
+bootstrap = Bootstrap5(app)
 
 app.config['SECRET_KEY'] = 'key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
